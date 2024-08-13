b0c5dd84b54108e23991a7e81a85106c8b39eae2
JordanGonz <jordan.grex23ok@gmail.com>
2024-08-13 14:20:01 -0500

    cambios

--- Scrum-API/Program.cs
+++ Scrum-API/Program.cs
@@ -1,20 +1,45 @@
+using Microsoft.EntityFrameworkCore;
+using Scrum.Infraestructure.MainContext;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+builder.Configuration
+    .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+    .AddJsonFile($"appsettings.{builder.Environment.EnvironmentName}.json", optional: true, reloadOnChange: true)
+    .AddEnvironmentVariables();
+
+builder.Logging.ClearProviders();
+builder.Logging.AddConsole();
+builder.Logging.AddFile(o => o.RootPath = o.RootPath = @"C:\Scrum_Logs");
+
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("ISCPolicy", policy =>
+    {
+        policy.AllowAnyOrigin()
+        .AllowAnyMethod()
+        .AllowAnyHeader();
+    });
+});
+
+
+builder.Services.AddDbContext<ScrumManagementContext>(options =>
+        options.UseSqlServer(builder.Configuration.GetConnectionString("ScrumDb")));
 
 app.UseHttpsRedirection();
 
---
+++ Scrum.Infraestructure/MainContext/ScrumManagementContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Scrum.Infraestructure.MainContext
+{
+    public partial class ScrumManagementContext : DbContext
+
+    {
+        public ScrumManagementContext(DbContextOptions options) : base(options)
+        {
+        }
+    }
+}
